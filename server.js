cccc17e0578b016b1ade520c679e4c7222bac1b3
mischa.huang <mischa.huang@creativearktech.com>
2022-04-14 21:13:08 +0800

    feat: Mongoose 新增 Model

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ mongoose.connect('mongodb://localhost:27017/hotel')
   .then(() => console.log('db connect success'))
   .catch(e => console.log(e));
 
+const RoomSchema = {
+  name: String,
+  price: {
+    type: Number,
+    required: [true, '價格必填']
+  },
+  rating: Number
+};
+
+const Room = mongoose.model('Room', RoomSchema);
+
 const requestListener = (req, res) => {
   console.log('req: ', req.url);
   res.end();
